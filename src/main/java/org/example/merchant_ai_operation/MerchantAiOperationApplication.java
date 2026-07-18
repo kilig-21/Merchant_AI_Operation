5f905bd171286756e46dad841e0a8a207a8b9d8b
李红鲜 <995677570@qq.com>
2026-07-18 13:46:59 +0800

    first commit

---
+++ src/main/java/org/example/merchant_ai_operation/MerchantAiOperationApplication.java
@@ -0,0 +1,13 @@
+package org.example.merchant_ai_operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantAiOperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantAiOperationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/merchant_ai_operation/MerchantAiOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.merchant_ai_operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantAiOperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
